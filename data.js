fcd079984d5de8375bc50cc11d9a066763507a17
Rahul Chhabrani <45403387+rahulchhabrani@users.noreply.github.com>
2022-01-31 10:28:02 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+function data(name, year, qualification){
+    userData = {
+        name: name,
+        Year: year,
+        Qualification: qualification,
+    }
+    return userData
+}
+
+module.exports = data
